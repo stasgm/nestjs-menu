d4ffc997c0895f6a78ce424b4b828f122bddb36a
Stanislau Shlyahtich <stanislau.shlyahtich@sprintingsoftware.com>
2023-06-07 22:32:50 +0300

    Remove menulines and Update schema

--- prisma/migrations/20230605092911_init/migration.sql
+++ prisma/migrations/20230605092911_init/migration.sql
@@ -26,18 +26,9 @@ CREATE TABLE "Menu" (
     "disabled" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "products" JSONB,
 
     CONSTRAINT "Menu_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "MenuLine" (
-    "id" SERIAL NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-    "menuId" INTEGER NOT NULL,
-    "productId" INTEGER NOT NULL,
-
-    CONSTRAINT "MenuLine_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -60,12 +51,6 @@ CREATE UNIQUE INDEX "_MenuToMenuCategory_AB_unique" ON "_MenuToMenuCategory"("A"
 
 -- CreateIndex
 CREATE INDEX "_MenuToMenuCategory_B_index" ON "_MenuToMenuCategory"("B");
-
--- AddForeignKey
-ALTER TABLE "MenuLine" ADD CONSTRAINT "MenuLine_menuId_fkey" FOREIGN KEY ("menuId") REFERENCES "Menu"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "MenuLine" ADD CONSTRAINT "MenuLine_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_MenuToMenuCategory" ADD CONSTRAINT "_MenuToMenuCategory_A_fkey" FOREIGN KEY ("A") REFERENCES "Menu"("id") ON DELETE CASCADE ON UPDATE CASCADE;
