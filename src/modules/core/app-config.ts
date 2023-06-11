9d46792e4df76e6ca138e1b29a58f00f55f4f63e
Stanislau Shlyahtich <stanislau.shlyahtich@sprintingsoftware.com>
2023-06-11 20:28:31 +0300

    Update ts config

--- src/modules/core/app-config.ts
+++ src/modules/core/app-config.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import * as config from 'config';
+import config from 'config';
 
 // import config from 'config';
 import { PGCredentials } from '../../types';
