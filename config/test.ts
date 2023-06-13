679d6f60e6e65ac9ef929ee250173ff5d6c1a4b0
Stanislau Shliakhtsich <stasgm@gmail.com>
2023-06-13 20:44:28 +0300

    Set ip for test server

--- config/test.ts
+++ config/test.ts
@@ -5,7 +5,7 @@ const config: IAppConfig = {
   nestPort: 3000,
   postgres: {
     dbname: 'testdb',
-    host: 'localhost',
+    host: '192.168.100.9',
     port: 5434,
     user: 'test_user',
     password: '1234',
