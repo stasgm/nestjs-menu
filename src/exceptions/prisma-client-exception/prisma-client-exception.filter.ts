e084c3fe9b96c48be0a065b9146dc48f04fb42fe
Stanislau Shliakhtsich <stasgm@gmail.com>
2023-06-18 20:05:43 +0300

    Format Prisma errors

--- src/exceptions/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/exceptions/prisma-client-exception/prisma-client-exception.filter.ts
@@ -1,4 +1,11 @@
-import { ArgumentsHost, BadRequestException, Catch, HttpStatus, NotFoundException } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ConflictException,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
@@ -10,22 +17,28 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
     const response = ctx.getResponse<Response>();
 
     switch (exception.code) {
-      case 'P2002': {
-        const status = HttpStatus.CONFLICT;
-        response.status(status).json({
-          statusCode: status,
-          message: exception.meta?.target?.[0]
-            ? `A record with the given '${exception.meta?.target[0]}' field value already exists`
-            : 'Unknown error',
+      case 'P2000': {
+        this.formatError(response, {
+          message: this.cleanUpException(exception),
+          statusCode: HttpStatus.BAD_REQUEST,
           error: BadRequestException.name,
         });
         break;
       }
+      case 'P2002': {
+        this.formatError(response, {
+          message: exception.meta?.target?.[0]
+            ? `A record with the given '${exception.meta?.target[0]}' field value already exists`
+            : 'Unknown error',
+          statusCode: HttpStatus.CONFLICT,
+          error: ConflictException.name,
+        });
+        break;
+      }
       case 'P2025': {
-        const status = HttpStatus.NOT_FOUND;
-        response.status(status).json({
-          statusCode: status,
-          message: exception.meta?.cause || 'Record not found.',
+        this.formatError(response, {
+          message: (exception.meta?.cause as string | undefined) || 'Record not found.',
+          statusCode: HttpStatus.NOT_FOUND,
           error: NotFoundException.name,
         });
         break;
@@ -36,5 +49,16 @@ export class PrismaClientExceptionFilter extends BaseExceptionFilter {
         break;
       }
     }
+  }
+
+  cleanUpException(exception: Error): string {
+    return exception.message.replaceAll('\n', '');
+  }
+
+  formatError(
+    response: Response,
+    { statusCode, message, error }: { statusCode: number; message: string; error: string },
+  ) {
+    response.status(statusCode).json({ statusCode, message, error });
   }
 }
